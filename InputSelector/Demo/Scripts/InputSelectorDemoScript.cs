da2450b9a0ce43c080d4575fd5ef819117adc055
William Whitehouse <williamswhitehouse@gmail.com>
2020-02-19 14:37:45 +0000

    Add demo files

---
+++ InputSelector/Demo/Scripts/InputSelectorDemoScript.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------- //
+// Author  : William Whitehouse / WSWhitehouse //
+// GitHub  : github.com/WSWhitehouse           //
+// Created : 19/02/2020                        //
+// Edited  : 19/02/2020                        // 
+// ------------------------------------------- //
+
+using UnityEngine;
+
+namespace WSWhitehouse.InputSelector.Demo
+{
+    public class InputSelectorDemoScript : MonoBehaviour
+    {
+        [InputSelector] public string inputOne;
+        [InputSelector] public string inputTwo;
+    }
+}
